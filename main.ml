0afa6623705a15e0cb023cc331de4feaf47a6c29
Theo5607 <theo67.jeu@gmail.com>
2024-02-02 11:07:05 +0100

    test

--- main.ml
+++ main.ml
@@ -1,85 +1,58 @@
-type prise = { x : float; y : float}    (*les coordonnées sont exprimées en m*)
-type graphe = (int*float) list array  (*array de liste d'adjacence avec des couples (voisin, poids) *)
+type prise = { x : float; y : float; diff : float; teta: float }
+type etat = Droite | Gauche
 
+(*prises de départ (pieds et mains), et prise de fin*)
+let pd = (0, 0, 0, 0);;
+let pf = 0;; 
 
-let t_p = [|{x = 0.; y = 0.}; {x = 1.; y = 1.}; {x = 5.; y = 2.}; {x=0.; y=2.}; {x=1.; y=3.}|]
+(*distance euclidienne*)
+let d_eucli a b =
+  sqrt((b.x -. a.x) ** 2. +. (b.y -. a.y) ** 2.)
 
-let heuristique (i:int) (j:int) (t_p: prise array) : float option =   (*renvoie un float correspondant à la difficulté d'un déplacement entre deux prises
-                                                                            ou None si le mouvement est impossible *)
-    let p1 = t_p.(i) and p2 = t_p.(j) in
-    let p = sqrt ((p2.x -. p1.x) ** 2. +. (p2.y -. p1.y) ** 2.) in
-    if p < 1.7 (*&& p2.y > p1.y*) then Some p else None
+(*récupérer la dernière prise*)
+let get_p b =
+  b |> List.hd |> fst
 
+(*récupérer le dernier état*)
+let get_e b =
+  b |> List.hd |> snd
 
-let init_graphe (t_p : prise array) : graphe =      (*initialise un graphe où seuls les sommets assez proches sont reliés et uniquement de bas en haut*)
-    let g = Array.make (Array.length t_p) [] in
-    for i = 0 to Array.length g - 1 do
-        for j = 0 to Array.length g - 1 do
-            if i <> j then begin
-                match (heuristique i j t_p) with
-                | Some x -> g.(i) <- (j, x)::g.(i)
-                | None -> ()
-            end
-        done;
-    done;
-    g
+(*fonction heuristique*)
+let h i j e1 e2 t =
+  let p1, p2 = t.(i), t.(j) in
+  let d = d_eucli p1 p2 in
+  if d < 1.7 then Some (d ** 2. *. (exp (p1.diff +. p2.diff) /. 2.))
+  else None
 
+let creer_graph n = 
+  Array.make n []
 
-let txt_to_tab file : prise array*(int option)*(int option)= (*parcours le fichier contenant les coordonnées des prises pour en faire un prise array et sortir la prise de début et de fin*)
+let remplit_graph g h t =
+  Array.iteri
+    (fun i a ->
+      Array.iteri
+        (fun j b ->
+          let diff = h i j Droite Gauche t in
+          match diff with
+          | Some x -> g.(i) <- (j, x, diff |> Option.get) :: g.(i)
+          | None -> ()
+        ) g
+    ) g
+
+let backtrack (t: prise array) : (int * etat) list * float =
+  let sol = ref [] in
+  let diff_min = ref max_float in
+  let rec aux diff =
+    if diff < !diff_min then 
+      if get_p sol = pf
+
+let txt_to_tab file : prise array = (*parcours le fichier contenant les coordonnées des prises pour en faire un prise array*)
     let f = open_in file in
-    let l = ref [] in
-    let i = ref (-1) in
-    let fin = ref None in
-    let deb = ref None in
-    begin
-        try
-          while true do
-            incr i;
-            match String.split_on_char ' ' (input_line f) with      
-            |[px; py; d] -> (l := {x = float_of_string px; y = float_of_string py}::(!l); if d= "0" then deb := Some !i else if d="5" then fin := Some !i )
+    let n = int_of_string (input_line f) in 
+    let t = Array.make n {x = 0.; y = 0.; diff = 0.; teta = 0.} in
+    for i=0 to n-1 do
+        match String.split_on_char ' ' (input_line f) with      
+            |[px; py; d; teta] -> (t.(i) <- {x = float_of_string px; y = float_of_string py; diff = float_of_string d; teta = float_of_string teta})
             |_ -> ()
-            
-
-
-          done
-        with End_of_file -> close_in f
-    end;
-    (Array.of_list !l),!deb,!fin
-
-let meilleur_chemin (g : graphe)  (d : int) (f : int) : int list option = (*renvoie une liste option (à l'envers) des sommets à emprunter pour aller du sommet d à f*)
-    let _, pred = Dijkstra.dijkstra g d in 
-    let rec aux s =
-        if s = d then Some [s]
-        else
-            match pred.(s) with
-            |None -> None
-            |Some s' -> match aux s' with |None -> None | Some l -> Some (s::l)
-    in aux f
-
-
-let chemin_to_aretes_liste (c : int list option) (a : prise array) =
-    let rec aux l f=
-        match l with 
-        |[] -> ()
-        |[_] -> ()
-        |i::j::q -> Printf.fprintf f "%f %f %f %f\n" a.(i).x a.(i).y a.(j).x a.(j).y; aux (j::q) f
-    in
-    match c with 
-    |None -> failwith "pas de chemin possible"
-    |Some l -> let f = open_out "click_detection/liste_aretes.txt" in aux l f; close_out f 
-
-
-
-let main () =
-    let t_p, d, f = txt_to_tab "click_detection/liste_prises.txt" in 
-    let g = init_graphe t_p in 
-    match d, f with
-    |None, _ | _, None -> failwith "erreur, pas de prise de départ ou d'arrivée"
-    |Some d', Some f' ->
-        chemin_to_aretes_liste (meilleur_chemin g d' f') t_p
-;;
-
-main ();
-print_string "Ocaml terminé";
-
-
+    done;
+    t
